dbb904b9e0ff005df33d695f46c0d2fefa6aea8f
Rafael2319b <kiznaiver1309@gmail.com>
2024-08-25 16:21:22 -0600

    Add files via upload

---
+++ Tarea formato de fecha y hora/Ejercicio3.js
@@ -0,0 +1,3 @@
+const fecha = new Date()
+const mes = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
+console.log(mes[fecha.getMonth()])
