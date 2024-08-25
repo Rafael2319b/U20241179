123f23cb554e35cf3bebe51934cf76c72ccdd673
Rafael2319b <kiznaiver1309@gmail.com>
2024-08-25 16:09:18 -0600

    Delete Ejercicio1.js

--- Ejercicio1.js
+++
@@ -1,4 +0,0 @@
-const fecha = new Date('2012-03-20 03:02:18')
-const opciones = { weekday: 'long', year:'numeric',month:'short', day:'numeric', hour:'numeric'}
-
-console.log(fecha.toLocaleDateString('en-us',opciones))
